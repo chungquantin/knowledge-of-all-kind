51baa0b449956debb0d1dd514c1de27f29d09903
Chung Quan Tin <tin@snowflake.so>
2022-06-10 01:10:08 +0700

    add docker-compose for postgres setup

--- scrapers/src/services/database.py
+++ scrapers/src/services/database.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python
+
+import psycopg2
+
+
 class DatabaseService:
+    database: str
+    user: str
+    password: str
+    host: str
+    port: str
     def __init__(self) -> None:
         pass
+    
+   
